3ab4f51102977c772df245ceb4bffcec029e86f3
Pawel Krezel <pawel.krezel@icloud.com>
2026-03-09 16:04:50 +0000

    login page has been styled

---
+++ wineadmin/static/wineadmin/js/login_helper_script.js
@@ -0,0 +1,8 @@
+let usrName = document.getElementById("id_username");
+let pswdField = document.getElementById("id_password");
+
+usrName.setAttribute("class", "commonStyle commonStyleAsBlock");
+usrName.setAttribute("placeholder", "username 👤");
+
+pswdField.setAttribute("placeholder", "password 🔑");
+pswdField.setAttribute("class", "commonStyle commonStyleAsBlock");
