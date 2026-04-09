558328a058dee42eb2c7e280a921c6f7bb0207c2
Pawel Krezel <pawel.krezel@icloud.com>
2026-04-09 23:52:52 +0100

    header added - visible only for logged in users

--- WineFinderV2/settings.py
+++ WineFinderV2/settings.py
@@ -121,7 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LOGIN_REDIRECT_URL = "wineadmin"
 LOGIN_URL = "login"
-LOGOUT_REDIRECT_URL = "workInProgress"
+LOGOUT_REDIRECT_URL = "search"
 
 
 # Internationalization
