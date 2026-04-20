337d3734c40db722f67cc47ceb97f56c1efa6055
Pawel Krezel <pawel.krezel@icloud.com>
2026-04-20 23:59:25 +0100

    updated the serializer to return slot allocation data as well

--- wineadmin/serializers.py
+++ wineadmin/serializers.py
@@ -3,6 +3,11 @@ from .models import Wine
 
 # This code was created with assistance of generative AI
 class WineAdminSerializer(serializers.ModelSerializer):
+    slots = serializers.SerializerMethodField()
+
     class Meta:
         model = Wine
         fields = '__all__'
+
+    def get_slots(self, obj):
+        return [slot.id for slot in obj.slots.all()]
